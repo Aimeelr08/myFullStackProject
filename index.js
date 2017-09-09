b5e0ecfb22e88cde6269dcc2f6c00a27b61b9208
Aimeelr <aimeelynnramirez@gmail.com>
2017-09-09 14:21:16 -0400

    js file to be read; that scripts on html

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict';
 
 // user require with a reference to bundle the file and use it in this file
- let example = require('./example');
- let user = require('./example');
+
 // load manifests
 let scripts = require('./assets/scripts/index.js');
 let styles = require('./assets/styles/index.scss');
@@ -10,6 +9,5 @@ let styles = require('./assets/styles/index.scss');
 module.exports = {
   scripts,
   styles,
-  example,
-  user
+
 };
