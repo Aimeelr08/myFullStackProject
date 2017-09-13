f383581ecc644a34313f46bfbcce9b126f4a8991
Aimeelr <aimeelynnramirez@gmail.com>
2017-09-12 23:18:32 -0400

    added

--- assets/scripts/list/ui.js
+++ assets/scripts/list/ui.js
@@ -1,23 +1,23 @@
- 'use strict';
-//  const app = require('../app.js');
-const showPostsTemplate = require('../templates/Posts-listing.handlebars');
-//
- const getPostsSuccess = () => {
-let showPostsHtml = showPostsTemplate(data)({ posts: data.posts });
-//   $('<td></td>').append(data);
- console.log(app.user);
-  };
+//  'use strict';
+// //  const app = require('../app.js');
+// const showPostsTemplate = require('../templates/Posts-listing.handlebars');
 // //
-// // //  const updatePostsSuccess = (data) => {
-// // //   $('.content').add(data);
-// // // };
+//  const getPostsSuccess = () => {
+// let showPostsHtml = showPostsTemplate(data)({ posts: data.posts });
+// //   $('<td></td>').append(data);
+//  console.log(app.user);
+//   };
+// // //
+// // // //  const updatePostsSuccess = (data) => {
+// // // //   $('.content').add(data);
+// // // // };
+// // //
+// //  const failure = (error) => {
+// //   console.error(error);
+// // };
 // //
-//  const failure = (error) => {
-//   console.error(error);
-// };
-//
-module.exports = {
- getPostsSuccess,
-// //   // updatePostsSuccess,
-//   failure,
-// };
+// module.exports = {
+//  getPostsSuccess,
+// // //   // updatePostsSuccess,
+// //   failure,
+// // };
